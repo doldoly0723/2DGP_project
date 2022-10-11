d434e9490b1860b9ce462d5b18ee7c5835f0539f
doldoly0723 <58391220+doldoly0723@users.noreply.github.com>
2022-10-12 01:25:02 +0900

    이동에 따른 머리 스프라이트 추가
    아이작 스프라이트를 좀 더 크게 할 필요가 있어보인다

--- isaac.py
+++ isaac.py
@@ -6,18 +6,30 @@ FULL_MAP_WID, FULL_MAP_HEI = 6401, 3600
 
 class Map:
     def __init__(self):
-        self.image = load_image('map.png')
+        self.image_map = load_image('map.png')
+        self.image_isaac = load_image('isaac.png')
         self.mid_x = MAP_WIDTH // 2
         self.mid_y = MAP_HEIGHT // 2
         self.map_x = FULL_MAP_WID // 2
         self.map_y = FULL_MAP_HEI // 2
+        self.head_x = 5
+        self.head_y = 900
+        self.head_WID = 40
+        self.head_HEI = 30
+        self.head_frame = 0
 
     def update(self):
+        # 키 입력에 따른 이동
         self.map_x += dir_x*5
         self.map_y += dir_y*5
+        self.head_frame = self.head_x + frame_head*40
+        # 키 입력에 따른 아이작 프레임 변화 머리, 다리 따로
         pass
     def draw(self):
-        self.image.clip_draw(self.map_x,self.map_y,MAP_WIDTH,MAP_HEIGHT,self.mid_x,self.mid_y)
+        self.image_map.clip_draw(self.map_x,self.map_y,MAP_WIDTH,MAP_HEIGHT,self.mid_x,self.mid_y)
+        # 머리
+        self.image_isaac.clip_draw(self.head_frame, self.head_y, self.head_WID, self.head_HEI, self.mid_x, self.mid_y)
+        # 몸
         pass
 
 # 캐릭터 이동 및 공격 키 입력
@@ -25,6 +37,7 @@ def handle_events():
     global running
     global dir_y
     global dir_x
+    global frame_head
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -32,6 +45,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
         elif event.type == SDL_KEYDOWN:
+            # 이동
             if event.key == SDLK_w:
                 dir_y += 1
             elif event.key == SDLK_s:
@@ -40,6 +54,15 @@ def handle_events():
                 dir_x -= 1
             elif event.key == SDLK_d:
                 dir_x += 1
+            #이동에 따른 아이작 스프라이트
+            elif event.key == SDLK_UP:
+                frame_head = 4
+            elif event.key == SDLK_DOWN:
+                frame_head = 0
+            elif event.key == SDLK_LEFT:
+                frame_head = 6
+            elif event.key == SDLK_RIGHT:
+                frame_head = 2
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_w:
                 dir_y -= 1
@@ -55,8 +78,12 @@ open_canvas(MAP_WIDTH,MAP_HEIGHT)
 
 
 running = True
-dir_x=0;
-dir_y=0;
+dir_x=0
+dir_y=0
+# 맵 이동에 따른 아이작 프레임 설정 머리 다리 따로 설정 필요
+frame_head = 0
+frame_bodyX = 0
+frame_bodyY = 0
 map = Map()
 
 while running:
