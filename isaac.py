5681e4ac83b44d776353631c8540ef387a0b7ebf
doldoly0723 <58391220+doldoly0723@users.noreply.github.com>
2022-10-12 00:31:17 +0900

    기존 맵 이동 Calss로 변환

--- isaac.py
+++ isaac.py
@@ -2,7 +2,23 @@ from pico2d import*
 # 화면 크기
 MAP_WIDTH, MAP_HEIGHT = 1600, 900
 # 전체 맵 크기
-FULL_MAP_WID, FULL_MAP_HEI = 7113, 4000
+FULL_MAP_WID, FULL_MAP_HEI = 6401, 3600
+
+class Map:
+    def __init__(self):
+        self.image = load_image('map.png')
+        self.mid_x = MAP_WIDTH // 2
+        self.mid_y = MAP_HEIGHT // 2
+        self.map_x = FULL_MAP_WID // 2
+        self.map_y = FULL_MAP_HEI // 2
+
+    def update(self):
+        self.map_x += dir_x*5
+        self.map_y += dir_y*5
+        pass
+    def draw(self):
+        self.image.clip_draw(self.map_x,self.map_y,MAP_WIDTH,MAP_HEIGHT,self.mid_x,self.mid_y)
+        pass
 
 # 캐릭터 이동 및 공격 키 입력
 def handle_events():
@@ -37,28 +53,19 @@ def handle_events():
 
 open_canvas(MAP_WIDTH,MAP_HEIGHT)
 
-isaac = load_image('isaac.png')
-MAP = load_image('map.png')
-
 
 running = True
 dir_x=0;
 dir_y=0;
-mid_x = MAP_WIDTH // 2
-mid_y = MAP_HEIGHT // 2
-map_x = FULL_MAP_WID // 2
-map_y = FULL_MAP_HEI // 2
+map = Map()
 
 while running:
     clear_canvas()
-    # 이미지의 좌표를 변경하며 맵이 이동하는 것처럼 보임
-    MAP.clip_draw(map_x,map_y,MAP_WIDTH,MAP_HEIGHT,mid_x,mid_y)
-    
+    handle_events()
+    map.update()
+    map.draw()
     update_canvas()
 
-    handle_events()
-    map_x += dir_x*5
-    map_y += dir_y*5
     delay(0.01)
 
 
