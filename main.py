0bc07aaefb370ed5a6011fd56890cb23c49c83e5
doldoly0723 <58391220+doldoly0723@users.noreply.github.com>
2022-10-15 01:44:46 +0900

    메인 함수 추가 후 리펙토링 완성

---
+++ main.py
@@ -0,0 +1,15 @@
+import pico2d
+import isaac
+
+pico2d.open_canvas(isaac.MAP_WIDTH,isaac.MAP_HEIGHT)
+
+isaac.enter()
+
+while isaac.running:
+    isaac.handle_events()
+    isaac.update()
+    isaac.draw()
+
+isaac.exit()
+
+pico2d.clear_canvas()
