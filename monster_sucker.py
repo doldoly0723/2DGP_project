a2424d9d835fad1b9bd41db31230c1f23b3af5c5
doldoly0723 <58391220+doldoly0723@users.noreply.github.com>
2022-11-30 23:13:57 +0900

    몬스터 sucker 수정
    플레이어에게 직선이동하는 코드 변경    속도 및 프레임 처리 완료

--- monster_sucker.py
+++ monster_sucker.py
@@ -1,10 +1,24 @@
 from pico2d import*
 
+import math
 import attack
+import game_framework
 import isaac
 import random   # 몬스터의 출현
 import playstate
 MAP_WIDTH, MAP_HEIGHT = 1600, 900
+
+PIXEL_PER_METER = (10.0 / 0.1)  #10 pixel 10cm
+RUN_SPEED_KMPH = 5.0   #Km / Hour
+
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+TIME_PER_ACTION = 10.0
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 2
+
 class Sucker:
     image = None
     reverse_image = None
@@ -31,6 +45,7 @@ class Sucker:
         self.choose_wall = 0 # 리스폰 지역 설정
 
         self.hp = 200
+        self.dir = 0
 
     def respawn_sucker(self):
         if self.monster_status == False:
@@ -54,40 +69,49 @@ class Sucker:
                 elif self.choose_wall == 4: #오른쪽
                     self.x = MAP_WIDTH
                     self.y = random.randint(0, MAP_HEIGHT)
+                # self.x = 500
+                # self.y = 500
                 self.monster_status = True
-        # self.sucker_x = 0
-        # self.sucker_y = 0
 
-        pass
+
+
 
     def update(self):
         Sucker.respawn_sucker(self)
         if self.monster_status == True:
-            #self.monster_t += 0.00001  # 시간 지날수록 속도 증가
-            self.monster_t = 0.005      # 속도 고정
+            # self.monster_t += 0.00001  # 시간 지날수록 속도 증가
+            # self.monster_t = 0.005      # 속도 고정
+            self.dir = math.atan2(playstate.player.mid_y - self.y, playstate.player.mid_x - self.x)
             if playstate.player.map_x == playstate.player.end_of_left or playstate.player.map_x == playstate.player.end_of_right:
-                self.x = ((1 - self.monster_t) * self.x + self.monster_t * playstate.player.mid_x)
+                self.x += RUN_SPEED_PPS * math.cos(self.dir)*game_framework.frame_time
             else:
-                self.x = ((1-self.monster_t)*self.x + self.monster_t*playstate.player.mid_x) - playstate.player.dir_x*5
+                self.x += RUN_SPEED_PPS * math.cos(self.dir)*game_framework.frame_time - playstate.player.dir_x*5
 
             if playstate.player.map_y == playstate.player.end_of_top or playstate.player.map_y == playstate.player.end_of_bottom:
-                self.y = ((1 - self.monster_t) * self.y + self.monster_t * playstate.player.mid_y)
+                self.y += RUN_SPEED_PPS * math.sin(self.dir)*game_framework.frame_time
             else:
-                self.y = ((1-self.monster_t)*self.y + self.monster_t*playstate.player.mid_y) - playstate.player.dir_y*5
+                self.y += RUN_SPEED_PPS * math.sin(self.dir)*game_framework.frame_time - playstate.player.dir_y*5
 
-        self.frame_count += 1
-        if self.frame_count == 10:
-            self.frame = (self.frame + 1) % 2
-            self.frame_count = 0
-            pass
+
+            # self.x += RUN_SPEED_PPS * math.cos(self.dir)*game_framework.frame_time
+            # self.y += RUN_SPEED_PPS * math.sin(self.dir)*game_framework.frame_time
+
+        self.frame = (self.frame + FRAMES_PER_ACTION + ACTION_PER_TIME + game_framework.frame_time) % FRAMES_PER_ACTION
+        # self.frame_count += 1
+        # if self.frame_count == 10:
+        #     self.frame = (self.frame + 1) % 2
+        #     self.frame_count = 0
+        #     pass
+
+
 
     def draw(self):
         if self.monster_status == True:
             if self.x <= self.reverse_x: # sucker 스프라이트 좌우 방향
-                self.image.clip_draw(self.frame*80, 0,
+                self.image.clip_draw(int(self.frame)*80, 0,
                                         self.WID, self.HEI, self.x, self.y)
             else:
-                self.reverse_image.clip_draw(self.frame * 80, 0,
+                self.reverse_image.clip_draw(int(self.frame) * 80, 0,
                                             self.WID, self.HEI, self.x, self.y)
             # draw_rectangle(*self.get_bb())
 
--- playstate.py
+++ playstate.py
@@ -41,19 +41,19 @@ def enter():
 
     game_world.add_object(player, 0)
 
-    # suckers = [Sucker() for i in range(3)]
-    # game_world.add_objects(suckers, 1)
+    suckers = [Sucker() for i in range(3)]
+    game_world.add_objects(suckers, 1)
     #
-    spittys = [Spitty() for i in range(3)]
-    game_world.add_objects(spittys, 1)
+    # spittys = [Spitty() for i in range(3)]
+    # game_world.add_objects(spittys, 1)
     #
     # # 몬스터와 공격 충돌체크
-    # game_world.add_collision_pairs(None, suckers, 'tears:suckers')
-    game_world.add_collision_pairs(None, spittys, 'tears:spittys')
+    game_world.add_collision_pairs(None, suckers, 'tears:suckers')
+    # game_world.add_collision_pairs(None, spittys, 'tears:spittys')
     #
     # #몬스터와 캐릭터 충돌 체크
-    # game_world.add_collision_pairs(player, suckers, 'player:suckers')
-    game_world.add_collision_pairs(player, spittys, 'player:spittys')
+    game_world.add_collision_pairs(player, suckers, 'player:suckers')
+    # game_world.add_collision_pairs(player, spittys, 'player:spittys')
     #
     # #몬스터 공격과 플레이어 충돌 체크
     # game_world.add_collision_pairs(None, player, 'monster_tears:player')
